2aa666776fb6fa3cc567c86151ad9a524ec59594
Justin Bender <jrkbender@gmail.com>
2018-02-05 19:21:41 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Enter in a position to the fibonacci function and it will return the number from that position.
+
+Using a memoized method
---
+++ script.js
@@ -0,0 +1,16 @@
+function fibonacciMemoized(position) {
+
+
+}
+
+
+console.log(fibonacciMemoized(4)); // will give 3
+
+console.log(fibonacciMemoized(9)); // will give 34
+
+console.log(fibonacciMemoized(12)); // will give 144
+
+console.log(fibonacciMemoized(20)); // will give 6765
+
+
+//Does it blow your mind how it works like mine...
