006a3805c0933324e8b809fa57507aec22ab3358
Justin Bender <jrkbender@gmail.com>
2018-02-10 23:28:02 -0500

    added the code

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-function fibonacciMemoized(position) {
-
-
+function fibMemo(index, cache) {
+  //index is the number in the fibonacci seq
+  // cache is the array of memory
+  cache = cache || [];
+  if(cache[index]) return cache[index];
+  else {
+    if(index < 3) return 1;
+    else {
+      cache[index] = fibMemo(index - 1, cache) + fibMemo(index - 2, cache);
+    }
+  }
+  return cache[index];
 }
 
 
-console.log(fibonacciMemoized(4)); // will give 3
+console.log(fibMemo(4)); // will give 3
 
-console.log(fibonacciMemoized(9)); // will give 34
+console.log(fibMemo(9)); // will give 34
 
-console.log(fibonacciMemoized(12)); // will give 144
+console.log(fibMemo(12)); // will give 144
 
-console.log(fibonacciMemoized(20)); // will give 6765
+console.log(fibMemo(20)); // will give 6765
 
 
 //Does it blow your mind how it works like mine...
